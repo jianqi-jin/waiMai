6a01102e537d7a2aa07dcff16f917b512580244d
2359634711 <2359634711@qq.com>
2019-07-20 10:56:56 +0800

    7-20reduceAdd
    7-20reduceAdd

--- components/reduceAdd/reduceAdd.js
+++ components/reduceAdd/reduceAdd.js
@@ -4,7 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    number: {
+      value: 0,
+      type: Number
+    }
   },
 
   /**
@@ -18,6 +21,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
-  }
+    onAction(e) {
+      let type = e.currentTarget.dataset.type;
+      if (type == 'add' || this.data.number > 0) {
+        this.triggerEvent('onAction', {
+          type
+        })
+      }
+    }
+  },
 })
--- pages/good/orderDetail/orderDetail.js
+++ pages/good/orderDetail/orderDetail.js
@@ -5,62 +5,125 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tempNum: 0,
+    posInfo: { //位置信息
+      iconImg: '/res/icon/icon-address@3x.png',
+      title: '台位：A区03号桌',
+      iconArr: '/res/icon/icon-right-little@3x.png',
+      value: '',
+      titleColor: 'rgba(102,102,102,1)',
+      titleStyle: "font-size:26rpx;",
+      valueColor: ''
+    },
+    infoList: [{ //订单信息
+      iconImg: '',
+      title: '备注',
+      iconArr: '/res/icon/icon-right-little@3x.png',
+      value: '口味、爱好等备注',
+      titleColor: '',
+      titleStyle: "",
+      valueColor: '',
+      valueStyle: 'color:rgba(204,204,204,1);'
+    }, {
+      iconImg: '',
+      title: '餐品数量',
+      iconArr: '',
+      value: 'x4',
+      titleColor: '',
+      titleStyle: "",
+      valueColor: '',
+      valueStyle: 'color:rgba(204,204,204,1);'
+    }, {
+      iconImg: '',
+      title: '优惠券',
+      iconArr: '',
+      value: '3个可使用',
+      titleColor: '',
+      titleStyle: "",
+      valueColor: '',
+      valueStyle: 'color:rgba(225,55,47,1);'
+    }, {
+      iconImg: '',
+      title: '余额抵扣',
+      iconArr: '',
+      value: '-￥32.3',
+      titleColor: '',
+      titleStyle: "",
+      valueColor: '',
+      valueStyle: 'color:rgba(225,55,47,1);'
+    }],
+    payInfo: {
+      title: '微信支付',
+      iconArr: '/res/icon/pj-ok@3x.png',
+      iconArrStyle: 'width:40rpx;height:40rpx'
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
+  onNumChange(e) {
+    console.log(e)
+    let type = e.detail.type;
+    if (type == 'add') {
+      this.setData({
+        tempNum: this.data.tempNum + 1
+      })
+    } else {
+      this.setData({
+        tempNum: this.data.tempNum - 1
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
