6f20d0cba28c6520592d99a3249223a4fafa81ac
2359634711 <2359634711@qq.com>
2019-07-20 15:26:44 +0800

    ww

--- pages/good/goodDetail/goodDetail.js
+++ pages/good/goodDetail/goodDetail.js
@@ -1,66 +1,17 @@
-// pages/good/goodDetail/goodDetail.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    commentTitle: {
+      title: "餐品评价",
+      iconArr: "/res/icon/icon-right-little@3x.png",
+      value: "24条评价",
+      titleStyle: "font-weight:800;font-size:36rpx",
+      valueStyle: "font-size:24rpx;color:rgba(153,153,153,1)",
+      style: "padding-top: 40rpx"
+    },
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ]
   }
 })
